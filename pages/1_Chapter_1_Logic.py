26bd9b8461585379d3f7c069081e165f7a02fd6c
Mani <mmani@umflint.edu>
2026-08-02 18:45:03 -0400

    Deleting old files

--- pages/1_Chapter_1_Logic.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Chapter 1: Logic", layout="wide")
-
-st.title("Chapter 1: Logic")
-st.subheader("Coming Soon")
-st.caption("This section is under development.")
